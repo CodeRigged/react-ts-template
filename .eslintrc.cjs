15f560d85ceda0c04ac767d5a0a7da1c8d83ac2b
CodeRigged <tantonovics@gmail.com>
2024-07-02 17:14:30 +0200

    add typescript-sort-keys dev dependency + update app title

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,12 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:typescript-sort-keys/recommended",
     "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "typescript-sort-keys"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
   },
