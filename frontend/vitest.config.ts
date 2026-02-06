c2443508f9226f87c06afe00c4719eecd5ba2c3f
CodeRigged <tantonovics@gmail.com>
2026-02-06 15:37:07 +0100

    add mock zustand for testing purposes and refactor vitest.config

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,27 +1,15 @@
-import react from "@vitejs/plugin-react"
-import path from "path"
-import { defineConfig } from "vitest/config"
+import { defineConfig, mergeConfig } from "vitest/config"
+import viteConfig from "./vite.config"
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/setupTests.ts",
-    coverage: {
-      reporter: ["text", "json", "html"],
-    },
-    maxConcurrency: 4,
-  },
-  resolve: {
-    alias: {
-      "~/components": path.resolve(__dirname, "src/components"),
-      "~/constans": path.resolve(__dirname, "src/constans"),
-      "~/layouts": path.resolve(__dirname, "src/layouts"),
-      "~/pages": path.resolve(__dirname, "src/pages"),
-      "~/stores": path.resolve(__dirname, "src/stores"),
-      "~/types": path.resolve(__dirname, "src/types"),
-      "~/utils": path.resolve(__dirname, "src/utils"),
-    },
-  },
-})
+export default defineConfig(() =>
+  mergeConfig(
+    viteConfig,
+    defineConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: ["./setup-vitest.ts"],
+      },
+    })
+  )
+)
