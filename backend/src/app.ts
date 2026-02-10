cba0dfcfaa386a11091128e3edfc9c74c746e6ae
CodeRigged <tantonovics@gmail.com>
2026-02-10 17:59:55 +0100

    [backend] fix: resolve path alias issues in test files and allow tests to run alongside dev

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,9 @@
-import logger from "@logger";
-import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import todoRoutes from "~/routes/todoRoutes";
 import { applyMiddleware } from "./app.middleware";
 
-dotenv.config();
-
-// MongoDB connection
-const MONGO_URI = `${process.env.MONGO_URI}`;
-
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 applyMiddleware(app);
 
 // Health check endpoint
@@ -26,18 +17,4 @@ app.get("/health", async (_req, res) => {
 // Todos API
 app.use("/todos", todoRoutes);
 
-// Connect to DB and start server
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    logger.info("Connected to MongoDB");
-    app.listen(PORT, () => {
-      logger.info(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    logger.error("MongoDB connection error:", err);
-    process.exit(1);
-  });
-
-export default app; // Export app for testing
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import logger from "@logger";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import app from "./app";
+
+dotenv.config();
+
+const MONGO_URI = `${process.env.MONGO_URI}`;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+    app.listen(PORT, () => {
+      logger.info(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    logger.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
