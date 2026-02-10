6841a2e63c88365d85b2c8677ecb2d5bf81709cb
CodeRigged <tantonovics@gmail.com>
2026-02-10 16:56:52 +0100

    add tsconfig-paths to frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,11 @@
 import react from "@vitejs/plugin-react"
-import path from "path"
 import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 9000,
   },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "~/components": path.resolve(__dirname, "src/components"),
-      "~/constans": path.resolve(__dirname, "src/constans"),
-      "~/layouts": path.resolve(__dirname, "src/layouts"),
-      "~/pages": path.resolve(__dirname, "src/pages"),
-      "~/stores": path.resolve(__dirname, "src/stores"),
-      "~/types": path.resolve(__dirname, "src/types"),
-      "~/utils": path.resolve(__dirname, "src/utils"),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 })
