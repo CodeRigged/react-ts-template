33b9692bc53e039672649065bbd623ba62e0b093
CodeRigged <tantonovics@gmail.com>
2026-02-10 18:22:35 +0100

    [backend] feat: include pinoOptions in pino-http middleware

--- backend/src/app.middleware.ts
+++ backend/src/app.middleware.ts
@@ -4,6 +4,17 @@ import cors from "cors";
 import { Express } from "express";
 import pino from "pino-http";
 
+const pinoOptions =
+  process.env.NODE_ENV === "test"
+    ? {
+        serializers: {
+          req: () => undefined,
+          res: () => undefined,
+          err: () => undefined,
+        },
+      }
+    : {};
+
 /**
  * Applies all global middleware to the Express app.
  */
@@ -11,5 +22,10 @@ export function applyMiddleware(app: Express) {
   app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(pino({ logger }));
+  app.use(
+    pino({
+      logger,
+      ...pinoOptions,
+    }),
+  );
 }
