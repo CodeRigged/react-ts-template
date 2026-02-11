4e98307150b9312050a2b01ec819885f4cf4c6f8
CodeRigged <tantonovics@gmail.com>
2026-02-11 16:12:12 +0100

    [frontend] fix: refactor eslint file to enable flat config and update packages

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,29 @@
+import eslint from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import perfectionist from "eslint-plugin-perfectionist"
+import reactHooks from "eslint-plugin-react-hooks"
+import { reactRefresh } from "eslint-plugin-react-refresh"
+import { defineConfig } from "eslint/config"
+import tseslint from "typescript-eslint"
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactRefresh.configs.recommended(),
+  reactHooks.configs.flat.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          type: "natural",
+          order: "asc",
+        },
+      ],
+    },
+  }
+)
